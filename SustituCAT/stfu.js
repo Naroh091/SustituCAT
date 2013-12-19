6e8479f8c3cf4dd009caeb2591d4619302f8474d
David Fernández <yo@naroh.es>
2013-12-19 16:39:25 +0100

    Nuevas frases
    Dedicado a Pilar Rahola y NacióDigital.cat

--- SustituCAT/stfu.js
+++ SustituCAT/stfu.js
@@ -40,8 +40,9 @@ function removeIndependentism(textNode) {
     v = v.replace(/\bconsulta soberanista\b/g, "gran adoración al bizcocho de chocolate");
     v = v.replace(/\bconsulta secesionista\b/g, "gran adoración al bizcocho de chocolate");
 	v = v.replace(/\bconsulta catalana\b/g, "gran adoración al bizcocho de chocolate");
-    v = v.replace(/\bindependent\b/g, "brillante como la purpurina");
-    v = v.replace(/\bIndependent\b/g, "brillante como la purpurina");
+	v = v.replace(/\bconsulta seccessionista\b/g, "gran adoració al pa de pessic de xocolata");
+    v = v.replace(/\bindependent\b/g, "brillant com la purpurina");
+    v = v.replace(/\bIndependent\b/g, "Brillant com la purpurina");
     v = v.replace(/\bindependiente\b/g, "brillante como la purpurina");
     v = v.replace(/\bIndependiente\b/g, "brillante como la purpurina");
     v = v.replace(/\bindependentista\b/g, "brillante como la purpurina");
@@ -55,10 +56,10 @@ function removeIndependentism(textNode) {
     v = v.replace(/\bla vía catalana\b/g, "el corro de la patata");
     v = v.replace(/\bvía catalana\b/g, "corro de la patata");
     v = v.replace(/\bvia catalana\b/g, "corro de la patata");
-    v = v.replace(/\bla via catalana\b/g, "el corro de la patata");
+    v = v.replace(/\bla via catalana\b/g, "el ball del rodó");
     v = v.replace(/\bla Vía Catalana\b/g, "el Corro de la Patata");
     v = v.replace(/\bVía Catalana\b/g, "Corro de la Patata");
-	v = v.replace(/\bVia Catalana\b/g, "Corro de la Patata");
+	v = v.replace(/\bVia Catalana\b/g, "Ball del Rodó");
 	v = v.replace(/\bindependentismo catalán\b/g, "conjuro mágico");
 	v = v.replace(/\bIndependentismo Catalán\b/g, "Conjuro Mágico");
 	v = v.replace(/\bIndependentismo catalán\b/g, "Conjuro mágico");
@@ -69,7 +70,10 @@ function removeIndependentism(textNode) {
 	v = v.replace(/\bArtur Mas\b/g, "Jefe Supremo de los Ponies");
 	v = v.replace(/\bDerecho a Decidir\b/g, "Derecho a comer pasteles");
 	v = v.replace(/\bderecho a decidir\b/g, "derecho a comer pasteles");
-	v = v.replace(/\bderecho a Decidir\b/g, "Derecho a comer pasteles");
+	v = v.replace(/\bderecho a Decidir\b/g, "derecho a comer pasteles"); 
+	v = v.replace(/\bdret a decidir\b/g, "Dret a menjar pastissos");
+	v = v.replace(/\bDret a Decidir\b/g, "dret a menjar pastissos");
+	v = v.replace(/\bdret a Decidir\b/g, "dret a menjar pastissos");
 	v = v.replace(/\bautodeterminacion\b/g, "automasturbación");
 	v = v.replace(/\bautodeterminación\b/g, "automasturbación");
 	v = v.replace(/\bgobierno catalan\b/g, "Consejo Mayor de Ancianos Danzarines");
@@ -90,10 +94,22 @@ function removeIndependentism(textNode) {
 	v = v.replace(/\bGeneralitat\b/g, "Generalísima");
 	v = v.replace(/\bDiada\b/g, "Piruletada");
 	v = v.replace(/\bEstado soberano\b/g, "Estado gaseoso");
+	v = v.replace(/\bEstado propio\b/g, "Estado gaseoso");
+	v = v.replace(/\bestado propio\b/g, "Estado gaseoso");
+	v = v.replace(/\bestado soberano\b/g, "Estado gaseoso");
 	v = v.replace(/\bveto a la consulta\b/g, "rechazo a la rica miel");
-	v = v.replace(/\brechazo a la consulta\b/g, "rechazo a la rica miel");
+	v = v.replace(/\brechazo a la consulta\b/g, "rechazo a la rica miel"); 
+	v = v.replace(/\brebuig a la consulta\b/g, "rebuig a la bona mel");
     v = v.replace(/\bsoberanista\b/g, "mágica");
     v = v.replace(/\bSoberanista\b/g, "mágica");
+	v = v.replace(/\bPilar Rahola\b/g, "Reina de las Croquetas, Pilar Rahola");
+	v = v.replace(/\bNacióDigital.cat\b/g, "NaciónDePlastilina.cat");
+	v = v.replace(/\bNació Digital\b/g, "Nación de plastilina");
+	v = v.replace(/\bnació digital\b/g, "Nación de plastilina");
+	v = v.replace(/\bnaciodigital.cat\b/g, "NacionDePlastilina.cat");
+	v = v.replace(/\bestat propi\b/g, "estat gasós");
+	v = v.replace(/\bestat propi\b/g, "estat gasós");	
+	v = v.replace(/\bEstat propi\b/g, "Estat gasós"); 
 		
 	textNode.nodeValue = v;
 }
